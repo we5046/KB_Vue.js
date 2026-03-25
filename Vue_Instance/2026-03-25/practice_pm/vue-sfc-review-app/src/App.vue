d346c9fcbed15c8b955aa6899c8fd92fa248e1f6
we5046 <we5046@naver.com>
2026-03-25 14:26:17 +0900

    [practice] first step - init setting files

---
+++ Vue_Instance/2026-03-25/practice_pm/vue-sfc-review-app/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container m-3">
+    <div class="card card-body p-4">
+      <p>안녕하세요 KB IT's Your Life 입니다.</p>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style scope></style>
