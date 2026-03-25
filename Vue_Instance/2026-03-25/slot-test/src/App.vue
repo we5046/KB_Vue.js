001374dc18d6c4e4b5405a01d70ac67c96322ee9
we5046 <we5046@naver.com>
2026-03-25 10:15:37 +0900

    practice setting with vue slot

---
+++ Vue_Instance/2026-03-25/slot-test/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NoSlotTest />
+    <SlotTest />
+  </div>
+</template>
+<script>
+import NoSlotTest from './components/NoSlotTest.vue';
+import SlotTest from './components/SlotTest.vue';
+export default {
+  name: 'App',
+  components: { NoSlotTest, SlotTest },
+};
+</script>
