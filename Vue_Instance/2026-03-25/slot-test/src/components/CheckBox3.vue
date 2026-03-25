3762ae00b7fa974c36314428497ed6b7c07818ba
we5046 <76059661+we5046@users.noreply.github.com>
2026-03-25 13:21:33 +0900

    Practice/2026 03 25/am (#22)
    * vue instance 모델 생성(style-test)
    
    * practice setting with vue slot
    
    * practice not using slot
    
    * practice using slot
    
    * practice using named slot
    
    * [practice] Final nonSlot - Slot - namedSlot
    
    * [practice] + addition Layout another Slot

---
+++ Vue_Instance/2026-03-25/slot-test/src/components/CheckBox3.vue
@@ -0,0 +1,19 @@
+<!-- Named Slot Test -->
+<template>
+  <div>
+    <slot name="icon"></slot>
+    <input
+      type="checkbox"
+      :value="id"
+      :checked="checked"
+      @change="$emit('check-changed', { id, checked: $event.target.checked })"
+    />
+    <slot name="label">Item</slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CheckBox3',
+  props: ['id', 'checked'],
+};
+</script>
