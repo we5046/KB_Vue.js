c16f91094b51aa4c7d41dd9bd023dd6ea4e9e453
we5046 <we5046@naver.com>
2026-03-25 09:40:00 +0900

    vue instance 모델 생성(style-test)

---
+++ Vue_Instance/2026-03-25/style-test/src/components/Child2.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="$style.child">
+    <h2>Child2</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Child2',
+  created() {
+    console.log(this.$style);
+  },
+};
+</script>
+<style module>
+.child {
+  background-color: blue; /* Child2는 blue, Child3는 orange */
+
+  border: solid 1px black;
+  margin: 1.5em;
+  padding: 1em;
+}
+</style>
