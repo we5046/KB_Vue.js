9779a3b15809514bdbe75f06e5239fddeb48f37f
we5046 <we5046@naver.com>
2026-03-27 13:23:13 +0900

    [practice] 오전타임 실습

---
+++ Vue_Instance/2026-03-27/calc-component-test/src/components/Calc.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    X : <input type="text" v-model.number="x" /><br />
+    Y : <input type="text" v-model.number="y" /><br />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'calc',
+  setup() {
+    const x = ref(10);
+    const y = ref(20);
+
+    x.value = 20;
+
+    console.log(x);
+    console.log(y);
+
+    return { x, y };
+  },
+};
+</script>
