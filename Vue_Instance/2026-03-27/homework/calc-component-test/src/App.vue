5d8ab6904291067e9ab52ee40ad2b25eee4f372b
we5046 <we5046@naver.com>
2026-03-27 17:42:11 +0900

    [homework] 기본 끝

--- Vue_Instance/2026-03-27/homework/calc-component-test/src/App.vue
+++ Vue_Instance/2026-03-27/homework/calc-component-test/src/App.vue
@@ -1,11 +1,20 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <!-- <Calc /> -->
+    <!-- <Calc2 /> -->
+    <!-- <Calc3 /> -->
+    <!-- <Calc4></Calc4> -->
+    <Calc5 />
+  </div>
 </template>
-
-<style scoped></style>
+<script>
+import Calc from './components/Calc.vue';
+import Calc2 from './components/Calc2.vue';
+import Calc3 from './components/Calc3.vue';
+import Calc4 from './components/Calc4.vue';
+import Calc5 from './components/Calc5.vue';
+export default {
+  name: 'App',
+  components: { Calc, Calc2, Calc3, Calc4, Calc5 },
+};
+</script>
---
+++ Vue_Instance/2026-03-27/homework/calc-component-test/src/components/Calc4.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    X : <input type="text" v-model.number="state.x" /> <br />
+    Y : <input type="text" v-model.number="state.y" /> <br />
+    <div>결과 : {{ result }}</div>
+  </div>
+</template>
+
+<script>
+import { reactive, computed } from 'vue';
+export default {
+  name: 'Calc4',
+  setup() {
+    const state = reactive({ x: 10, y: 20 });
+    const result = computed(() => state.x + state.y);
+
+    return { state, result };
+  },
+};
+</script>
